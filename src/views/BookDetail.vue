c319134c6f416ee9fbdece44038cde0a216f8acb
丰杰 <fengjie@aixuexi.com>
2020-11-17 20:12:08 +0800

    '修改样式'

--- src/views/BookDetail.vue
+++ src/views/BookDetail.vue
@@ -1,23 +1,68 @@
 <template>
   <el-container>
     <el-main>
-      <el-row style="height: 200px;text-align: center;">
-        <h1>{{book.bookName}}</h1>
-        <p>书籍作者：{{book.bookWriter}}</p>
-        <p>书记类型：{{book.bookType}}</p>
-        <p>点击次数：{{book.bookClickCount}}</p>
-        <p v-if="book.isFavorite">是否喜欢：是</p>
-        <p v-else>是否喜欢：否</p>
+      <el-row style="height: 200px; text-align: center">
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span>{{ book.bookName }}</span>
+          </div>
+          <el-col :span="9"
+            ><p>书籍作者：{{ book.bookWriter }}</p></el-col
+          >
+          <el-col :span="9"
+            ><p>书记类型：{{ book.bookType }}</p></el-col
+          >
+          <el-col :span="9"
+            ><p>点击次数：{{ book.bookClickCount }}</p></el-col
+          >
+          <el-col :span="9"
+            ><p v-if="book.isFavorite">是否喜欢：是</p>
+            <p v-else>是否喜欢：否</p></el-col
+          >
+        </el-card>
+        <!-- <h1>{{ book.bookName }}</h1>
+        <el-col :span="9"
+          ><p>书籍作者：{{ book.bookWriter }}</p></el-col
+        >
+        <el-col :span="9"
+          ><p>书记类型：{{ book.bookType }}</p></el-col
+        >
+        <el-col :span="9"
+          ><p>点击次数：{{ book.bookClickCount }}</p></el-col
+        >
+        <el-col :span="9"
+          ><p v-if="book.isFavorite">是否喜欢：是</p>
+          <p v-else>是否喜欢：否</p></el-col
+        > -->
       </el-row>
-      <el-row style="height: 400px">
+      <el-divider></el-divider>
+      <el-row
+        style="
+          height: 400px;
+          margin-top: 20px;
+          padding-left: 10%;
+          padding-right: 10%;
+        "
+      >
         <div v-if="book.chapters != null">
-          <el-col v-for="chapter in book.chapters"
-                  v-bind:key="chapter.id">
-            <router-link :to="{name:'BookReader',params:{bookId:chapter.boodId,chapterId:chapter.id}}"
-                         >{{chapter.chapterName}}</router-link>
+          <el-col
+            :span="6"
+            v-for="chapter in book.chapters"
+            v-bind:key="chapter.id"
+          >
+            <el-tag type="success">
+              <el-link type="primary">
+                <router-link
+                  :to="{
+                    name: 'BookReader',
+                    params: { bookId: chapter.boodId, chapterId: chapter.id },
+                  }"
+                  >{{ chapter.chapterName }}</router-link
+                ></el-link
+              >
+            </el-tag>
           </el-col>
         </div>
-
       </el-row>
     </el-main>
   </el-container>
@@ -50,5 +95,7 @@ export default {
 </script>
 
 <style scoped>
-
+.el_row {
+  margin-top: 20px;
+}
 </style>>
--- src/views/BookReader.vue
+++ src/views/BookReader.vue
@@ -1,17 +1,29 @@
 <template>
   <el-container>
-    <el-main style="width: 50%;background-color:yellow;">
-      <p v-for="(line,index) in chapter.lines"
-         v-bind:key="index">
-        {{line.line}}<el-tag @click="handleComment(line.line,chapter.id)">{{line.commitNum}}</el-tag>
+    <el-main class="read_main">
+      <p v-for="(line, index) in chapter.lines" v-bind:key="index">
+        {{ line.line }}
+        <!-- <el-tag @click="handleComment(line.line, chapter.id)"></el-tag> -->
+        <el-button
+          @click="handleComment(line.line, chapter.id)"
+          type="primary"
+          style="margin-left: 16px"
+          size="small"
+        >
+          <!-- {{line.commitNum}} -->1
+        </el-button>
       </p>
+
+      <el-drawer title="我是标题" :visible.sync="drawer" :with-header="false">
+        <span>我来啦!</span>
+      </el-drawer>
     </el-main>
-    <el-aside style="width:40%;background-color:green;">
+    <!-- <el-aside style="width:40%;background-color:green;">
         <p v-for="comment in comments"
            :key="comment.id">
           {{comment.content}}
         </p>
-    </el-aside>
+    </el-aside> -->
   </el-container>
 </template>
 
@@ -22,6 +34,7 @@ export default {
     return {
       chapter: {},
       comments: [],
+      drawer: false,
       // commentData: null,
     };
   },
@@ -38,6 +51,7 @@ export default {
         });
     },
     handleComment(line, id) {
+      this.drawer = true;
       console.log(line, id);
       axios
         .get("/api/book-reader/barrage/listBarrage", {
@@ -60,4 +74,9 @@ export default {
 </script>
 
 <style scoped>
+.read_main {
+  /* width: 50%; */
+  background-color: #ffffcc;
+  margin: 10%;
+}
 </style>>
